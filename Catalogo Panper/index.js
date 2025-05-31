46ecaae03b483263cd8cfcbf47e261e65dbab954
Oigorr <166768551+Oigorr@users.noreply.github.com>
2025-05-31 12:53:20 -0300

    Add files via upload

---
+++ Catalogo Panper/index.js
@@ -0,0 +1,29 @@
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.getElementById("closeModal");
+
+// Abrir modal com animação
+document.querySelectorAll('.container-doors img').forEach(img => {
+    img.addEventListener('click', () => {
+        modalImg.src = img.src;
+        modal.style.display = "flex";
+        setTimeout(() => modal.classList.add("show"), 10);
+    });
+});
+
+// Fechar modal com animação
+function closeModal() {
+    modal.classList.remove("show");
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 300); // tempo da animação
+}
+
+closeBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
